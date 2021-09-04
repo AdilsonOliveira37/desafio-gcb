cd3f610e10e48f9e06e49168034bb277b3654c5e
Adilson Oliveira <aquatroolhos@gmail.com>
2021-09-04 03:47:22 -0300

    signup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,19 +1,40 @@
-
+import illustrationImg from '../../assets/images/signup.svg';
 import styles from './styles.module.scss';
 
 export function Signup() {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando um app de comidas saudaveis" />
+        <strong>Crie Salas ao vivo</strong>
+        <p>Can you be happy</p>
+      </aside>
+      <main>
+        <h1>Healthy Food</h1>
         <form>
+          <div className={styles.inputGroup}>
+            <label>Nome e sobrenome:</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputGroup}>
+            <label>Email:</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputGroup}>
+            <label>Data de nascimento:</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputGroup}>
+            <label>Nome:</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inputGroup}>
+            <label>Nome:</label>
+            <input type="text" />
+          </div>
           <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <button type="submit">Submit</button>
         </form>
-      </div>
+      </main>
     </div>
   )
 }
